121bb34b9a6e574f706818c237efb562e8bdfdda
Mohd Aadil Rana <aadilrana088@gmail.com>
2022-09-02 00:31:24 +0530

    Navbar and sidebar UI done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,16 @@
 import './home.scss';
+import Sidebar from '../../component/sidebar/Sidebar';
+import Navbar from '../../component/navbar/Navbar';
+
 const Home = () => {
-    return <div className="home">Home</div>;
+    return (
+        <div className="home">
+            <Sidebar />
+            <div className="homeContainer">
+                <Navbar />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
