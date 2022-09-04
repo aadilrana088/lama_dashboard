73b058077b830f0e06e5825cbdadb726f41cbe47
Mohd Aadil Rana <aadilrana088@gmail.com>
2022-09-04 20:04:05 +0530

    Added datatable from mui and customize it

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,6 +1,17 @@
+import Datatable from '../../component/datatable/Datatable';
+import Navbar from '../../component/navbar/Navbar';
+import Sidebar from '../../component/sidebar/Sidebar';
 import './list.scss';
 const List = () => {
-    return <div>List</div>;
+    return (
+        <div className="list">
+            <Sidebar />
+            <div className="listContainer">
+                <Navbar />
+                <Datatable />
+            </div>
+        </div>
+    );
 };
 
 export default List;
